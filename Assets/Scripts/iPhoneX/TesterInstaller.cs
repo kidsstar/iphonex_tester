1d061882760580657aa49134421aa540976a023b
Tetsuya Mori <monry84@gmail.com>
2017-10-11 20:37:16 +0900

    :sparkles::name_badge: Change class name and change I/F
    * クラス名を TesterInstaller に変更
    * メソッドの戻り値として AsyncOperation を返す
      * 読み込み完了を待てるようになります

---
+++ Assets/Scripts/iPhoneX/TesterInstaller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace iPhoneX {
+
+    public static class TesterInstaller {
+
+        /// <summary>
+        /// インストールする
+        /// </summary>
+        public static AsyncOperation Install() {
+#if UNITY_EDITOR
+            return SceneManager.LoadSceneAsync("iPhoneX_Tester", LoadSceneMode.Additive);
+#else
+            return null;
+#endif
+        }
+
+        /// <summary>
+        /// アンインストールする
+        /// </summary>
+        public static AsyncOperation Uninstall() {
+#if UNITY_EDITOR
+            return SceneManager.UnloadSceneAsync("iPhoneX_Tester");
+#else
+            return null;
+#endif
+        }
+
+    }
+
+}
