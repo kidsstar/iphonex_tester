040620061229bfb66cedf54ffadb8e3d47a56cb2
Tetsuya Mori <monry84@gmail.com>
2017-11-10 02:09:58 +0900

    :sparkles: Implement automatic destroy scene
    * iPhoneX_Tester シーンが読み込まれたら、DontDestroyOnLoad フラグを立てる
    * 自身のシーンは不要になるので Unload する

---
+++ Assets/Scripts/iPhoneXTester/Controller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace iPhoneXTester {
+
+    public class Controller : MonoBehaviour {
+
+        private const string SCENE_NAME = "iPhoneX_Tester";
+
+        private void Awake() {
+            this.gameObject.name = SCENE_NAME;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        private void Start() {
+            SceneManager.UnloadSceneAsync(SCENE_NAME);
+        }
+
+    }
+
+}
